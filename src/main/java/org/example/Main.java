1401724077a1a622fc03111b21ea7ed7c803f8f4
wadimbap <vadim345plotnikov@gmail.com>
2024-06-21 11:07:07 +0300

    Добавил переносы строк для читаемости

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,9 +6,32 @@ import java.util.concurrent.TimeUnit;
 public class Main {
     public static void main(String[] args) {
         CrptApi.Document.Description description = new CrptApi.Document.Description("string");
-        CrptApi.Document.Product product = new CrptApi.Document.Product("string", "2020-01-23", "string", "string", "string", "2020-01-23", "string", "string", "string");
+        CrptApi.Document.Product product = new CrptApi.Document.Product(
+                "string",
+                "2020-01-23",
+                "string",
+                "string",
+                "string",
+                "2020-01-23",
+                "string",
+                "string",
+                "string"
+        );
         CrptApi.Document.Product[] products = {product};
-        CrptApi.Document document = new CrptApi.Document(description, "string", "string", "LP_INTRODUCE_GOODS", true, "string", "string", "string", "2020-01-23", "string", products, "2020-01-23", "string");
+        CrptApi.Document document = new CrptApi.Document(description,
+                "string",
+                "string",
+                "LP_INTRODUCE_GOODS",
+                true,
+                "string",
+                "string",
+                "string",
+                "2020-01-23",
+                "string",
+                products,
+                "2020-01-23",
+                "string"
+        );
 
         CrptApi api = new CrptApi(TimeUnit.SECONDS, 5);
         try {
